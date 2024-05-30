95d87913bd23c88978c27cc8b544c4543e80b12c
GuedesThi <thiagodesignguedes@gmail.com>
2024-05-30 15:33:30 -0300

    Mudando URL para do Render

--- src/main.ts
+++ src/main.ts
@@ -28,7 +28,8 @@ const keyMap: { [key in keyof Endereco]: string } = {
 /*------------------------------------------------------------------------------------------------------*/
 
 async function comunicao_api(cep: string) {
-  const url: string = 'http://localhost:4545/?cep=' + cep;
+  //const url: string = 'http://localhost:4545/?cep=' + cep;
+  const url: string = 'https://api-myaddress.onrender.com/?cep=' + cep;
 
   try {
     const response = await fetch(url, {
